0383866988cc48ece073028a7e944b8a7f663fc8
Ebdollah <l201165@lhr.nu.edu.pk>
2025-03-14 14:23:56 +0500

    changed cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['*'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
