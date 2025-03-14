422a0b1bc7a473b167e6abd4393e52e3c34a23ce
Ebdollah <l201165@lhr.nu.edu.pk>
2025-03-14 15:13:27 +0500

    changed cors

--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,14 @@ export const app = express();
 
 app.use(
   cors({
-    origin: ['*'],
-    // methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-
+    origin: "*", // Allow all domains (if no credentials needed)
+    // origin: "http://localhost:3000", // Replace with your frontend URL in development
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Allow cookies & auth headers
   })
 );
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
